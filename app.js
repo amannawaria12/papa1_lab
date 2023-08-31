14fab1b6f9ce97d5dc1d7f42edf34dac00597633
aman nawaria <amannawaria12@gmail.com>
2023-08-30 23:15:52 -0500

    app.js

--- app.js
+++ app.js
@@ -1,8 +1,31 @@
 const express = require('express')
 const app = express()
+const port = (process.env.PORT || 3000)
 
-app.get('/', function (req, res) {
-  res.send('<h1>Hello World to express</h1>')
+app.set('view engine', 'ejs');
+
+
+
+let myVariableServer = 'soft coded server data';
+
+app.get('/aman', function (req, res) {
+  res.render('index', 
+  {
+    'myVariableClient' : myVariableServer 
+  }
+  );
 })
 
-app.listen(3000)
+app.get('/', function (req, res) {
+  res.send('<h1>Hello World From Express</h1>')
+})
+
+app.get('/whatever', function (req, res) {
+  res.sendFile(__dirname + '/index.html');
+})
+
+
+
+// app.listen(3000)
+
+app.listen(port, () => console.log(`Server is on ${ port }` ));
