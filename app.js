51f29dbfb04f20cf5b8f59bc3bddf612c9c592cd
aman nawaria <amannawaria12@gmail.com>
2023-09-19 13:29:51 -0500

    new update

--- app.js
+++ app.js
@@ -3,13 +3,43 @@ const app = express()
 const bodyParser = require('body-parser')
 const port = (process.env.PORT || 3000)
 
+
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://amannawaria12:i7ArOtzognEYkQJY@cluster0.tdtkr9c.mongodb.net/?retryWrites=true&w=majority";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
+
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
 let myVariableServer = 'soft coded server data';
 
-app.get('/aman', function (req, res) {
+app.get('/aman', async function (req, res) {
   res.render('index', 
   {
     'myVariableClient' : myVariableServer 
